c639d1339258959255cdf8efed0bb73843f16bcf
Ethan Hathaway <erhathaway@gmail.com>
2019-10-04 20:52:32 -0700

    Wip: Redo all types in terms of schemas

--- src/types.ts
+++ src/types.ts
@@ -5,29 +5,38 @@ export type BaseResolver<Node = any, P = undefined, A = undefined, C = {}, I = {
     info?: I
 ) => Node | Promise<Node>;
 
-export interface INodeChange {
-    userId: number;
-    userRoles?: string[];
-    revisionId: number;
-    revisionData: string;
-    revisionTime: number;
-    nodeSchemaVersion: number;
-    resolverOperation: string;
-    nodeName: string;
-}
+// tslint:disable
+export type Unpacked<T> = T extends (infer U)[]
+    ? U
+    : T extends (...args: any[]) => infer U
+    ? U
+    : T extends Promise<infer U>
+    ? U
+    : T;
+// tslint:enable
 
-export interface ILinkChange {
-    edgeNodeId: number;
-    edgeNodeName: string;
-    resolverOperation: string;
-    revisionTime: number;
-}
+// export interface INodeChange {
+//     userId: number;
+//     userRoles?: string[];
+//     revisionId: number;
+//     revisionData: string;
+//     revisionTime: number;
+//     nodeSchemaVersion: string;
+//     resolverOperation: string;
+//     nodeName: string;
+// }
+
+// export interface ILinkChange {
+//     edgeNodeId: number;
+//     edgeNodeName: string;
+//     resolverOperation: string;
+//     revisionTime: number;
+// }
 
 export interface IVersionConnection<Node> {
     edges: Array<{
         cursor: string;
-        nodeChange?: INodeChange;
-        linkChange?: ILinkChange;
+        version: IGqlVerisonNode;
         node: Node;
     }>;
     pageInfo: {
@@ -37,66 +46,284 @@ export interface IVersionConnection<Node> {
         endCursor: string;
     };
 }
-export interface INamesConfig {
-    tableNames?: {
-        event?: string;
-        eventImplementorType?: string;
-        eventNodeChange?: string;
-        eventNodeChangeFragment?: string;
-        eventLinkChange?: string;
 
-        role?: string;
-        userRole?: string;
-        nodeSnapshot?: string;
-    };
-    columnNames?: {
-        eventId?: string;
-        eventTime?: string;
-        eventUserId?: string;
-        eventNodeName?: string;
-        eventNodeId?: string;
-        eventResolverOperation?: string;
+/**
+ *
+ *  SQL Tables
+ *
+ */
 
-        eventImplementorTypeId?: string;
-        eventImplementorType?: string;
-
-        linkChangeId?: string;
-        linkChangeNodeNameA?: string;
-        linkChangeNodeIdA?: string;
-        linkChangeNodeNameB?: string;
-        linkChangeNodeIdB?: string;
-
-        nodeChangeId?: string;
-        nodeChangeRevisionData?: string;
-        nodeChangeNodeSchemaVersion?: string;
-
-        nodeChangeFragmentId?: string;
-        nodeChangeFragmentTime?: string;
-        nodeChangeFragmentParentNodeId?: string;
-        nodeChangeFragmentParentNodeName?: string;
-        nodeChangeFragmentChildNodeId?: string;
-        nodeChangeFragmentChildNodeName?: string;
-
-        snapshotId?: string;
-        snapshotTime?: string;
-        snapshotData?: string;
-        snapshotNodeSchemaVersion?: string;
-
-        roleId?: string;
-        roleName?: string;
-
-        userRoleId?: string;
-    };
+export interface ISqlEventTable {
+    id: string;
+    created_at: string;
+    user_id: string;
+    node_name: string;
+    node_id: string;
+    resolver_operation: string;
+    implementor_type_id: number;
 }
 
-export interface INamesForTablesAndColumns {
-    tableNames: {
-        [tableName in keyof Required<Required<INamesConfig>['tableNames']>]: string;
-    };
-    columnNames: {
-        [columnName in keyof Required<Required<INamesConfig>['columnNames']>]: string;
-    };
+export const DEFAULT_COLUMN_NAMES_EVENT_TABLE: {[k in keyof ISqlEventTable]: string} = {
+    id: 'id',
+    created_at: 'created_at',
+    user_id: 'user_id',
+    node_name: 'node_name',
+    node_id: 'node_id',
+    resolver_operation: 'resolver_operation',
+    implementor_type_id: 'implementor_type_id'
+};
+
+export interface ISqlEventImplementorTypeTable {
+    id: string;
+    type: string;
 }
+
+export const DEFAULT_COLUMN_NAMES_EVENT_IMPLEMENTOR_TYPE_TABLE: {
+    [k in keyof ISqlEventImplementorTypeTable]: string;
+} = {
+    id: 'id',
+    type: 'type'
+};
+
+export interface ISqlEventLinkChangeTable {
+    id: number;
+    event_id: number;
+    node_name_a: string;
+    node_id_a: string;
+    node_name_b: string;
+    node_id_b: string;
+}
+
+export const DEFAULT_COLUMN_NAMES_EVENT_LINK_CHANGE_TABLE: {
+    [k in keyof ISqlEventLinkChangeTable]: string;
+} = {
+    id: 'id',
+    event_id: 'event_id',
+    node_name_a: 'node_name_a',
+    node_id_a: 'node_id_a',
+    node_name_b: 'node_name_b',
+    node_id_b: 'node_id_b'
+};
+
+export interface ISqlEventNodeChangeTable {
+    id: number;
+    event_id: number;
+    revision_data: string;
+    schema_version: number;
+}
+
+export const DEFAULT_COLUMN_NAMES_EVENT_NODE_CHANGE_TABLE: {
+    [k in keyof ISqlEventNodeChangeTable]: string;
+} = {
+    id: 'id',
+    event_id: 'event_id',
+    revision_data: 'revision_data',
+    schema_version: 'schema_version'
+};
+
+export interface ISqlEventNodeFragmentChangeTable {
+    id: number;
+    created_at: string;
+    parent_node_id: string;
+    parent_node_name: string;
+    child_node_id: string;
+    child_node_name: string;
+}
+
+export const DEFAULT_COLUMN_NAMES_EVENT_NODE_FRAGMENT_CHANGE_TABLE: {
+    [k in keyof ISqlEventNodeFragmentChangeTable]: string;
+} = {
+    id: 'id',
+    created_at: 'created_at',
+    parent_node_id: 'parent_node_id',
+    parent_node_name: 'parent_node_name',
+    child_node_id: 'child_node_id',
+    child_node_name: 'child_node_name'
+};
+
+export interface ISqlRoleTable {
+    id: number;
+    role: string;
+}
+
+export const DEFAULT_COLUMN_NAMES_ROLE_TABLE: {[k in keyof ISqlRoleTable]: string} = {
+    id: 'id',
+    role: 'role'
+};
+
+export interface ISqlUserRoleTable {
+    id: number;
+    role_id: number;
+    user_id: number;
+}
+
+export const DEFAULT_COLUMN_NAMES_USER_ROLE_TABLE: {[k in keyof ISqlUserRoleTable]: string} = {
+    id: 'id',
+    role_id: 'role_id',
+    user_id: 'user_id'
+};
+
+export interface ISqlNodeSnapshotTable {
+    id: number;
+    created_at: string;
+    snapshot: string;
+    node_schema_version: number;
+}
+
+export const DEFAULT_COLUMN_NAMES_SNAPSHOT_TABLE: {[k in keyof ISqlNodeSnapshotTable]: string} = {
+    id: 'id',
+    created_at: 'created_at',
+    snapshot: 'snapshot',
+    node_schema_version: 'node_schema_version'
+};
+
+export type SqlTable<T> = {[k in keyof T]: string};
+
+export interface ISqlColumnNames {
+    event: SqlTable<ISqlEventTable>;
+    event_implementor_type: SqlTable<ISqlEventImplementorTypeTable>;
+    event_link_change: SqlTable<ISqlEventLinkChangeTable>;
+    event_node_change: SqlTable<ISqlEventNodeChangeTable>;
+    event_node_fragment_change: SqlTable<ISqlEventNodeFragmentChangeTable>;
+    role: SqlTable<ISqlRoleTable>;
+    user_role: SqlTable<ISqlUserRoleTable>;
+    node_snapshot: SqlTable<ISqlNodeSnapshotTable>;
+}
+
+export const DEFAULT_SQL_TABLE_NAMES: {[k in keyof ISqlColumnNames]: string} = {
+    event: 'event',
+    event_implementor_type: 'event_implementor_type',
+    event_link_change: 'event_link_change',
+    event_node_change: 'event_node_change',
+    event_node_fragment_change: 'event_node_fragment_change',
+    role: 'role',
+    user_role: 'user_role',
+    node_snapshot: 'node_snapshot'
+};
+
+/**
+ * Graphql Schema
+ */
+
+export interface IGqlVersionNodeBase {
+    id: string;
+    createdAt: number;
+    userId: string;
+    nodeName: string;
+    nodeId: string;
+    resolverOperation: string;
+    type: string;
+    userRoles: string[];
+}
+
+export interface IGqlVersionNodeChangeNode extends IGqlVersionNodeBase {
+    id: string;
+    createdAt: number;
+    userId: string;
+    nodeName: string;
+    nodeId: string;
+    resolverOperation: string;
+    type: string;
+
+    revisionData: string;
+    nodeSchemaVersion: string;
+}
+
+export interface IGqlVersionLinkChangeNode extends IGqlVersionNodeBase {
+    id: string;
+    createdAt: number;
+    userId: string;
+    nodeName: string;
+    nodeId: string;
+    resolverOperation: string;
+    type: string;
+
+    linkNodeId: string;
+    linkNodeName: string;
+}
+
+export type IGqlVerisonNode = IGqlVersionNodeChangeNode | IGqlVersionLinkChangeNode;
+
+/**
+ * Data access layer
+ */
+
+// ISqlEventImplementorTypeTable &
+//     ISqlEventTable &
+//     ISqlEventLinkChangeTable &
+//     ISqlEventNodeChangeTable &
+//     ISqlEventNodeFragmentChangeTable &
+//     ISqlRoleTable &
+//     ISqlUserRoleTable &
+//     ISqlNodeSnapshotTable;
+
+export type EventNodeChangeInsert = Partial<
+    ISqlEventTable & ISqlEventNodeChangeTable & {roles: Array<ISqlRoleTable['role']>}
+>;
+export type EventLinkChangeInsert = Partial<ISqlEventTable & ISqlEventLinkChangeTable>;
+export type NodeSnapshotInsert = Partial<ISqlNodeSnapshotTable>;
+export type EventNodeFragmentChangeInsert = Partial<ISqlEventNodeFragmentChangeTable>;
+
+// export interface INamesConfig {
+//     tableNames?: {
+//         event?: string;
+//         eventImplementorType?: string;
+//         eventLinkChange?: string;
+//         eventNodeChange?: string;
+//         eventNodeChangeFragment?: string;
+
+//         role?: string;
+//         userRole?: string;
+//         nodeSnapshot?: string;
+//     };
+//     columnNames?: {
+//         eventId?: string;
+//         eventTime?: string;
+//         eventUserId?: string;
+//         eventNodeName?: string;
+//         eventNodeId?: string;
+//         eventResolverOperation?: string;
+
+//         eventImplementorTypeId?: string;
+//         eventImplementorType?: string;
+
+//         linkChangeId?: string;
+//         linkChangeNodeNameA?: string;
+//         linkChangeNodeIdA?: string;
+//         linkChangeNodeNameB?: string;
+//         linkChangeNodeIdB?: string;
+
+//         nodeChangeId?: string;
+//         nodeChangeRevisionData?: string;
+//         nodeChangeNodeSchemaVersion?: string;
+
+//         nodeChangeFragmentId?: string;
+//         nodeChangeFragmentTime?: string;
+//         nodeChangeFragmentParentNodeId?: string;
+//         nodeChangeFragmentParentNodeName?: string;
+//         nodeChangeFragmentChildNodeId?: string;
+//         nodeChangeFragmentChildNodeName?: string;
+
+//         snapshotId?: string;
+//         snapshotTime?: string;
+//         snapshotData?: string;
+//         snapshotNodeSchemaVersion?: string;
+
+//         roleId?: string;
+//         roleName?: string;
+
+//         userRoleId?: string;
+//     };
+// }
+
+// export interface INamesForTablesAndColumns {
+//     tableNames: {
+//         [tableName in keyof Required<Required<INamesConfig>['tableNames']>]: string;
+//     };
+//     columnNames: {
+//         [columnName in keyof Required<Required<INamesConfig>['columnNames']>]: string;
+//     };
+// }
 
 export type UnPromisify<T> = T extends Promise<infer U> ? U : T;
 
@@ -118,57 +345,47 @@ export type InfoArgs<T> = T extends (
     ? A
     : undefined;
 
-export interface IRevisionInput {
-    // id: string;
-    userId: string;
-    userRoles?: string[];
-    revisionData: string;
-    revisionTime: string;
-    nodeSchemaVersion: number;
-    resolverOperation: string;
-    nodeName: string;
-    nodeId?: string | number;
-}
+// export interface IRevisionInput {
+//     // id: string;
+//     userId: string;
+//     userRoles?: string[];
+//     revisionData: string;
+//     revisionTime: string;
+//     nodeSchemaVersion: string;
+//     resolverOperation: string;
+//     nodeName: string;
+//     nodeId?: string | number;
+// }
 
-export interface INodeBuilderRevisionInfo {
-    revisionData: string;
-    revisionTime: string;
-    revisionId: number;
-    snapshotData?: string;
-    nodeSchemaVersion: number;
-    nodeName: string;
-    nodeId: string | number;
-    resolverOperation?: string;
-}
+// export interface INodeBuilderRevisionInfo {
+//     revisionData: string;
+//     revisionTime: string;
+//     revisionId: number;
+//     snapshotData?: string;
+//     nodeSchemaVersion: string;
+//     nodeName: string;
+//     nodeId: string | number;
+//     resolverOperation?: string;
+// }
 
-export interface IRevisionQueryResult<RevisionTime = string> {
-    revisionId: number;
-    revisionTime: RevisionTime;
-    revisionData: string;
+// export interface IRevisionQueryResult<RevisionTime = string> {
+//     revisionId: number;
+//     revisionTime: RevisionTime;
+//     revisionData: string;
 
-    nodeName: string;
-    nodeSchemaVersion: number;
-    userId: number;
-    nodeId: string | number;
-    resolverOperation: string;
+//     nodeName: string;
+//     nodeSchemaVersion: string;
+//     userId: number;
+//     nodeId: string | number;
+//     resolverOperation: string;
 
-    snapshotTime?: string;
-    snapshotData?: string;
+//     snapshotTime?: string;
+//     snapshotData?: string;
 
-    userRoles?: string[];
-}
+//     userRoles?: string[];
+// }
 
-export interface ITransformInput {
-    columnNames: NonNullable<INamesForTablesAndColumns['columnNames']> & {[column: string]: any};
-    columnData: NonNullable<INodeChange> & {[column: string]: any};
-}
-
-// tslint:disable
-export type Unpacked<T> = T extends (infer U)[]
-    ? U
-    : T extends (...args: any[]) => infer U
-    ? U
-    : T extends Promise<infer U>
-    ? U
-    : T;
-// tslint:enable
+// export interface ITransformInput {
+//     columnNames: NonNullable<INamesForTablesAndColumns['columnNames']> & {[column: string]: any};
+//     columnData: NonNullable<INodeChange> & {[column: string]: any};
+// }
