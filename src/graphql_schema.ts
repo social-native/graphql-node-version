61fbca77a4f88658e7f0df6376bfb81adcaf055f
Ethan Hathaway <erhathaway@gmail.com>
2019-10-10 15:34:22 -0700

    Fix: build

--- src/graphql_schema.ts
+++ src/graphql_schema.ts
@@ -1,6 +1,5 @@
 import unixTimeSec from '@social-native/snpkg-graphql-scalar-unix-time-sec';
 import {EVENT_IMPLEMENTOR_TYPE_IDS} from './enums';
-import {Resolver} from '../dev/types';
 
 const contentFormatToGraphqlTypeName: {[K in keyof typeof EVENT_IMPLEMENTOR_TYPE_IDS]: string} = {
     NODE_CHANGE: 'VersionNodeChange',
@@ -66,11 +65,7 @@ type VersionNodeLinkChange implements Version {
 `;
 
 // tslint:disable-next-line
-const __resolveType: Resolver<string | undefined, {type: EVENT_IMPLEMENTOR_TYPE_IDS}> = async ({
-    type
-}: {
-    type: EVENT_IMPLEMENTOR_TYPE_IDS;
-}) => {
+const __resolveType = async ({type}: {type: EVENT_IMPLEMENTOR_TYPE_IDS}) => {
     const graphqlTypeName = contentFormatToGraphqlTypeName[type];
 
     if (!graphqlTypeName) {
