1aa412fe4cf7cab2e5219b5df58b1573b8f4f904
Ethan Hathaway <erhathaway@gmail.com>
2019-10-16 13:35:23 -0700

    Feat: change lib migration over to adding migration file

--- src/bin.ts
+++ src/bin.ts
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
 
-import {createRevisionMigrations} from './index';
+// import {createRevisionMigrations} from './index';
 import yargs from 'yargs';
 import path from 'path';
 import fs from 'fs';
-
+import Bluebird from 'bluebird';
 import * as _kenxBin from './lib/knex/init-knex'; // tslint:disable-line
 
 // const {up, down} = createRevisionMigrations();
@@ -33,21 +33,83 @@ yargs.command({
 
         const knex = (kenxBin as any).initKnex(Object.assign({}, env), opts);
         // const list = await knex.migrate.latest({tableName: 'knex_migration_versions'});
-        const versionFileName = await knex.migrate.make('$_create_graphql_node_version_tables');
-        const versionFileNamePath = path.resolve(knex.migrate.config.directory, versionFileName);
-        const migrationContents = createRevisionMigrations(
-            undefined,
-            knex.migrate.config.extension
-        );
-        fs.writeFile(versionFileNamePath, migrationContents.migration, err => {
-            if (err) {
-                throw err;
-            }
+        // const versionFileNamePath = path.resolve(knex.migrate.config.directory, versionFileName);
+        // const migrationContents = createRevisionMigrations(
+        //     undefined,
+        //     knex.migrate.config.extension
+        // );
 
-            console.log('Migration saved!');
+        // const existingMigrations = [] as string[];
+        const existingMigrations = fs.readdirSync(knex.migrate.config.directory);
+        // , (_err: any, files: string[]) => {
+        // existingMigrations.forEach(file => {
+        //     console.log('EXISTING MIGRATION:', file);
+        //     existingMigrations.push(file);
+        // });
+        // // });
+        // console.log('EXISTING MIGRATION: ', fileName);
+
+        // let filenames = [];
+        const libMigrationDir = path.resolve('./src/migrations');
+        console.log('libMigrationDir', libMigrationDir);
+
+        const matchMigration = /(.*).(ts|js)/;
+
+        const newMigrations = [] as Array<{migrationName: string; fileName: string}>;
+        const libMigrationFiles = fs.readdirSync(libMigrationDir);
+        // , (_err: any, files: string[]) => {
+        libMigrationFiles.forEach(fileName => {
+            console.log('LOOKING AT EXISTING MIGRATION: ', fileName);
+
+            const match = fileName.match(matchMigration);
+            const potentialMigrationName = match && match[1];
+            if (potentialMigrationName) {
+                const matchingMigration = existingMigrations.find(m =>
+                    m.includes(potentialMigrationName)
+                );
+
+                if (matchingMigration === undefined) {
+                    console.log('EXISTING MIGRATION MATCHES?: ', 'NO');
+                    newMigrations.push({fileName, migrationName: potentialMigrationName});
+                } else {
+                    console.log('EXISTING MIGRATION MATCHES?: ', 'YES');
+                }
+            }
         });
+        // });
+        console.log('NEW MIGRATIONS: ', newMigrations);
+
+        const LIB_NAME = 'graphql_node_version';
+        await Bluebird.mapSeries(newMigrations, async ({migrationName, fileName}) => {
+            const newMigrationFileNamePath = await knex.migrate.make(
+                `${LIB_NAME}_${migrationName}`
+            );
+            console.log('MADE A NEW MIGRATION FILE AT: ', newMigrationFileNamePath);
+
+            const libMigrationFilePath = path.resolve(libMigrationDir, fileName);
+            console.log('Importing lib migration file at path: ', libMigrationFilePath);
+
+            const migrationFile = require(libMigrationFilePath);
+            console.log(
+                '********',
+                migrationFile.default()(LIB_NAME, knex.migrate.config.extension)
+            );
+            fs.writeFile(
+                newMigrationFileNamePath,
+                migrationFile.default()(LIB_NAME, knex.migrate.config.extension),
+                err => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    console.log('Migration saved!');
+                }
+            );
+        });
+        // newMigrations.forEach();
+
         // console.log(versionFileNamePath);
-        console.log(knex.migrate);
+        // console.log(knex.migrate);
         // // console.log('LIST', list);
         // await up(knex);
         // console.log('Created revision table'); // tslint:disable-line
